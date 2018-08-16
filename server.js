118b03820c8480ee833e36b668788252c49f9d5c
conor byrne <conor@vedra.io>
2018-08-16 12:55:20 +0100

    changed port

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var config = {
       "USER"    : "",
       "PASS"    : "",
       "HOST"    : "ec2-18-217-114-25.us-east-2.compute.amazonaws.com",
-      "PORT"    : "27017",
+      "PORT"    : "8080",
       "DATABASE" : "memoryBox"
     };
 
