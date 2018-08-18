29be5f5b85cb1d1ad3003732e40766b5cfaf8aa5
conor byrne <conor@vedra.io>
2018-08-18 15:28:26 +0100

    port chnage

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 9000,
+  port = process.env.PORT || 3000,
   mongoose = require('mongoose'),
   Task = require('./api/models/memoryBoxModel'), //created model loading here
   bodyParser = require('body-parser');
